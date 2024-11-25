9e25beca5c53f0d0c2c59334f0d3da39df5d4f51
Ankit Rajput <a7949@axtria.com>
2024-11-25 22:21:04 +0530

    ..

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app=FastAPI()
 # Allow React app on localhost:3000 to communicate with the FastAPI backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Can replace with specific frontend URL, like ["http://localhost:3000"]
+    allow_origins=["*"],  # Can replace with specific frontend URL, like ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods like GET, POST, etc.
     allow_headers=["*"],  # Allows all headers
