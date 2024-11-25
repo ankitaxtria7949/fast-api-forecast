20cb0f025c8798d7c8899e68205e9bbc9e9041ed
ankitaxtria7949 <A7949@axtria.com>
2024-11-25 22:45:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from pydantic import BaseModel
-import json
-from LinearRegression import LinearRegression
-from datetime import datetime
 from fastapi import File, UploadFile, Form
 from typing import Optional
 import csv
 from io import StringIO
+
+from LinearRegression import LinearRegression
 from LogLinearRegression import log_linear_regression
-from Naive import naive_forecast
-from SeasonalNaive import seasonal_naive_forecast
-from Average import average_forecast
-from ETS import ETS
 
 app=FastAPI()
 # Allow React app on localhost:3000 to communicate with the FastAPI backend
@@ -48,13 +40,5 @@ async def forecast_sales(file:UploadFile=File(...), selectedSheet: Optional[str]
         forecast_val, dt = LinearRegression(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
     elif selectedSheet == 'Log Linear Regression':
         forecast_val, dt = log_linear_regression(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
-    elif selectedSheet == 'Seasonal Naive':
-        forecast_val, dt = seasonal_naive_forecast(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)  
-    elif selectedSheet == "Naive":
-        forecast_val, dt = naive_forecast(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
-    elif selectedSheet == "Average":
-        forecast_val, dt = average_forecast(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
-    elif selectedSheet == "Additive Trend-Additive Seasonality":
-        forecast_val, dt = ETS(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
     return {"forecast": forecast_val, "dt": dt,"filename": file.filename, "historyFromDate" : historyFromDate,"historyToDate" : historyToDate,"selectedFromDate" : selectedFromDate,"selectedToDate" : selectedToDate}
 
