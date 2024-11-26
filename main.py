f75918b55d3e7bbd22346f57e6fd7bcddff7a1e7
Ankit Rajput <a7949@axtria.com>
2024-11-26 10:40:30 +0530

    ...

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import File, UploadFile, Form
+from typing import Optional
+import csv
+from io import StringIO
+
 
 app=FastAPI()
 # Allow React app on localhost:3000 to communicate with the FastAPI backend
@@ -10,7 +15,7 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods like GET, POST, etc.
     allow_headers=["*"],  # Allows all headers
 )
- 
+
 
  
  
@@ -20,7 +25,14 @@ app.add_middleware(
 def read_root():
     return {"message": "Hello World"}
 
-@app.post("/demo")
-def demo():
-    return {"message":"Hellow"}
+
+@app.post("/upload")
+async def forecast_sales(file:UploadFile=File(...), selectedSheet: Optional[str] = Form(None), historyFromDate: Optional[str] = Form(None), historyToDate: Optional[str] = Form(None), selectedFromDate: Optional[str] = Form(None), selectedToDate: Optional[str] = Form(None)): 
+    contents = await file.read()
+    decoded = contents.decode('utf-8')
+    csv_reader = csv.reader(StringIO(decoded), delimiter=',')
+    data = [row for row in csv_reader]
+    
+    return {"message": "Hello World"}
+
 
